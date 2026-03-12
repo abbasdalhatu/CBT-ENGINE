5141870449a1b7d7bff5641f759d3969a1efae7d
abbasdalhatu <abbasdlht@gmail.com>
2026-03-12 15:34:22 -0700

    Add check_answer and finish_test to TestEngine

--- engine.py
+++ engine.py
@@ -20,6 +20,7 @@ class TestEngine:
         self.total_questions = 0
         self.start_time = None
         self.end_time = None
+        self.current_question = None
 
     def load_questions(self, questions_list):
         """
@@ -36,5 +37,21 @@ class TestEngine:
         Returns None if the queue is empty.
         """
         if self.question_queue:
-            return self.question_queue.popleft()
+            self.current_question = self.question_queue.popleft()
+            return self.current_question
+        self.current_question = None
         return None
+
+    def check_answer(self, user_answer):
+        """
+        Checks the user's answer against the current question's correct answer.
+        Updates the score if correct.
+        """
+        if self.current_question and user_answer == self.current_question.answer:
+            self.score += 1
+
+    def finish_test(self):
+        """
+        Marks the end time of the test using datetime.now().
+        """
+        self.end_time = datetime.now()
