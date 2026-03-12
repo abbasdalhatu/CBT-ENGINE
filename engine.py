3357fba2d6f5cb192f6cc3b8268da81960ed9b79
abbasdalhatu <abbasdlht@gmail.com>
2026-03-12 15:32:25 -0700

    Implement Question class

--- engine.py
+++ engine.py
@@ -1,5 +1,17 @@
+import collections
+from datetime import datetime
+
 class Question:
-    pass
+    def __init__(self, id, text, options, answer):
+        """
+        Encapsulates a single test question.
+        options is a list of strings.
+        answer is the correct option string.
+        """
+        self.id = id
+        self.text = text
+        self.options = options
+        self.answer = answer
 
 class TestEngine:
     pass
